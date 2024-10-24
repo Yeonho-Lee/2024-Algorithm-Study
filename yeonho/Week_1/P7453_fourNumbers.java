28fdf19b207a75991ce37ba60b681369a4cc3bc7
Lee Yeon Ho <55523393+Yeonho-Lee@users.noreply.github.com>
2024-10-24 21:52:06 +0900

    Yeonho Week 5 (#29)

--- yeonho/Week_1/P7453_fourNumbers.java
+++ yeonho/Week_1/P7453_fourNumbers.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
@@ -7,7 +8,7 @@ public class P7453_fourNumbers {
     static int n;
     static int[] A, B, C, D;
 
-    public static void main(String[] args) throws IOsException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
