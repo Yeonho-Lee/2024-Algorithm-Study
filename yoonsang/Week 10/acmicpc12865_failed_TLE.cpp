540c1b9f5ad6bbcf69c19204dd519673bc9c11ed
qe51951 <winter-flower@naver.com>
2024-11-26 21:09:07 +0900

    Week 10
    하루한문제

---
+++ yoonsang/Week 10/acmicpc12865_failed_TLE.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int n, k, a[101][2], weight=0, value=0, maxvalue=0;
+bool collected[101]={};
+void backtracking(int d){
+    if(d==n) return;
+    for(int i=d;i<n;i++){
+        if(collected[i]) continue;
+        if(weight+a[i][0]>k) continue;
+        collected[i]=true; weight+=a[i][0]; value+=a[i][1];
+        maxvalue=max(maxvalue, value);
+        backtracking(d+1);
+        collected[i]=false; weight-=a[i][0]; value-=a[i][1];
+    }
+}
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> n >> k;
+    for(int i=0;i<n;i++) {
+        cin >> a[i][0] >> a[i][1];
+    }
+    backtracking(0);
+    cout << maxvalue;
+}
---
+++ yoonsang/Week 10/acmicpc1967.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <array>
+using namespace std;
+int n, temp[3], maxsum=0;
+vector<array<int, 2>> v[10001];
+int dfs(int node){
+    if(v[node].size()==0) return 0;
+    int nodesum=0, nodemax=0, max1=0, max2=0;
+    for(auto i:v[node]){
+        int a=0;
+        if(v[i[0]].size()!=0) a=dfs(i[0]);
+        nodemax=max(nodemax, a+i[1]);
+        if(max1<=a+i[1]) { max2=max1; max1=a+i[1]; }
+        else if(max2<=a+i[1]) max2=a+i[1];
+    }
+    nodesum=max1+max2;
+    maxsum=max(nodesum, maxsum);
+    return nodemax;
+}
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> n;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<3;j++) cin >> temp[j];
+        v[temp[0]].push_back({temp[1], temp[2]}); // next, cost
+    }
+    int c=dfs(1);
+    cout << max(maxsum, c);
+}
