1f89a3abcc895d123763caaf61eb9605ef1a41d0
Lee Yeon Ho <55523393+Yeonho-Lee@users.noreply.github.com>
2024-12-25 23:09:29 +0900

    Yeonho Week 10, 12, 13 (#42)
    * Yeonho Week10
    
    * Yeonho Week12
    
    * Yeonho Week10
    
    * Yeonho Week12
    
    * Yeonho Week13

---
+++ yeonho/Week_12/P12865_normalBackpack2.java
@@ -0,0 +1,45 @@
+package yeonho.Week_12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P12865_normalBackpack2 {
+    static int N, K;
+    static int[] W, V;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        W = new int[N + 1];
+        V = new int[N + 1];
+
+        dp = new int[K + 1];
+        // dp[i][k] -> i번째 물건까지 고려했을 때, 배낭의 허용 무게가 k일 때의 최대 가치
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
+        }
+
+        knapsack();
+        System.out.println(dp[K]);
+
+    }
+
+    static void knapsack() {
+        for (int i = 1; i <= N; i++) {
+            for (int k = K; k >= W[i]; k--) {
+                // dp[k] = 배낭의 허용무게가 k일 때 최대 가치
+                dp[k] = Math.max(dp[k], V[i] + dp[k - W[i]]);
+            }
+        }
+    }
+}
---
+++ yeonho/Week_12/README.md
@@ -0,0 +1,3 @@
+# 2225번 합분해
+
+[백준 2225 자바 - 합분해 (BOJ 2225 JAVA)](https://nahwasa.com/entry/%EB%B0%B1%EC%A4%80-2225-%EC%9E%90%EB%B0%94-%ED%95%A9%EB%B6%84%ED%95%B4-BOJ-2225-JAVA)
